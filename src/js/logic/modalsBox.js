4c6741c9dc0ea7bec3854f0f43a53d52bdc1b1ee
Ahmad Fauzan <fauzan18296@gmail.com>
2024-11-29 22:23:15 +0700

    add: +update semua modal box models components

--- src/js/logic/modalsBox.js
+++ src/js/logic/modalsBox.js
@@ -30,37 +30,120 @@ export function modalBox2() {
 }
 
 export function modalBox3() {
-  
+  const openBtn3 = document.getElementById("open-modal-3")
+  const closeBtn3 = document.getElementById("close-modal-3")
+  const modal3 = document.getElementById("modal-3")
+
+  modal3.classList.add("invisible")
+  openBtn3.addEventListener("click", () => {
+    modal3.classList.remove("invisible")
+  })
+  closeBtn3.addEventListener("click", () => {
+    modal3.classList.add("invisible")
+  })
 }
 
 export function modalBox4() {
-
+  const openBtn4 = document.getElementById("open-modal-4")
+  const closeBtn4 = document.getElementById("close-modal-4")
+  const modal4 = document.getElementById("modal-4")
+  modal4.classList.add("invisible")
+  openBtn4.addEventListener("click", () => {
+    modal4.classList.remove("invisible")
+  })
+  closeBtn4.addEventListener("click", () => {
+    modal4.classList.add("invisible")
+  })
 }
 
 export function modalBox5() {
-
+  const openBtn5 = document.getElementById("open-modal-5")
+  const closeBtn5 = document.getElementById("close-modal-5")
+  const modal5 = document.getElementById("modal-5")
+  modal5.classList.add("invisible")
+  openBtn5.addEventListener("click", () => {
+    modal5.classList.remove("invisible")
+  })
+  closeBtn5.addEventListener("click", () => {
+    modal5.classList.add("invisible")
+  })
 }
 
 export function modalBox6() {
-
+  const openBtn6 = document.getElementById("open-modal-6")
+  const closeBtn6 = document.getElementById("close-modal-6")
+  const modal6 = document.getElementById("modal-6")
+  modal6.classList.add("invisible")
+  openBtn6.addEventListener("click", () => {
+    modal6.classList.remove("invisible")
+  })
+  closeBtn6.addEventListener("click", () => {
+    modal6.classList.add("invisible")
+  })
 }
 
 export function modalBox7() {
-
+  const openBtn7 = document.getElementById("open-modal-7")
+  const closeBtn7 = document.getElementById("close-modal-7")
+  const modal7 = document.getElementById("modal-7")
+  modal7.classList.add("invisible")
+  openBtn7.addEventListener("click", () => {
+    modal7.classList.remove("invisible")
+  })
+  closeBtn7.addEventListener("click", () => {
+    modal7.classList.add("invisible")
+  })
 }
 
 export function modalBox8() {
-
+ const openBtn8 = document.getElementById("open-modal-8")
+  const closeBtn8 = document.getElementById("close-modal-8")
+  const modal8 = document.getElementById("modal-8")
+  modal8.classList.add("invisible")
+  openBtn8.addEventListener("click", () => {
+    modal8.classList.remove("invisible")
+  })
+  closeBtn8.addEventListener("click", () => {
+    modal8.classList.add("invisible")
+  })
 }
 
 export function modalBox9() {
-
+  const openBtn9 = document.getElementById("open-modal-9")
+  const closeBtn9 = document.getElementById("close-modal-9")
+  const modal9 = document.getElementById("modal-9")
+  modal9.classList.add("invisible")
+  openBtn9.addEventListener("click", () => {
+    modal9.classList.remove("invisible")
+  })
+  closeBtn9.addEventListener("click", () => {
+    modal9.classList.add("invisible")
+  })
 }
 
 export function modalBox10() {
-
+  const openBtn10 = document.getElementById("open-modal-10")
+  const closeBtn10 = document.getElementById("close-modal-10")
+  const modal10 = document.getElementById("modal-10")
+  modal10.classList.add("invisible")
+  openBtn10.addEventListener("click", () => {
+    modal10.classList.remove("invisible")
+  })
+  closeBtn10.addEventListener("click", () => {
+    modal10.classList.add("invisible")
+  })
 }
 
 export function modalBox11() {
-  
+  const openBtn11 = document.getElementById("open-modal-11")
+  const closeBtn11 = document.getElementById("close-modal-11")
+  const modal11 = document.getElementById("modal-11")
+
+  modal11.classList.add("invisible")
+  openBtn11.addEventListener("click", () => {
+    modal11.classList.remove("invisible")
+  })
+  closeBtn11.addEventListener("click", () => {
+    modal11.classList.add("invisible")
+  })
 }
